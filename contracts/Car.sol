4378e8208db4f2a44f426b69590ad775223b6ef3
Michal Szulc <michalion91@gmail.com>
2023-10-16 00:28:55 +0200

    owner can transfer the car

--- contracts/Car.sol
+++ contracts/Car.sol
@@ -1,16 +1,30 @@
-//SPDX-License-Identifier: UNLICENSED
-
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 // This is the main building block for smart contracts.
 contract Car {
     address public manufacturer;
-    uint256[] public cars;
     mapping(uint256 => address) public owners;
 
     mapping(uint256 => address) public buyers;
 
     constructor() {
         manufacturer = msg.sender;
+    }
+
+    function transfer(uint256 car, address newOwner) public {
+        address currentOwner = owners[car];
+
+        if (currentOwner == address(0)) {
+            currentOwner = manufacturer;
+        }
+
+        require(currentOwner == msg.sender, "Car owner only");
+
+        owners[car] = newOwner;
+    }
+
+    function getOwner(uint256 car) public view returns (address) {
+        return owners[car];
     }
 }
--- test/Car.js
+++ test/Car.js
@@ -5,12 +5,26 @@ const {
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
 describe("Car contract", () => {
-  it("sets the manufacturer as the owner of the contract", async () => {
-    const { testedContract, manufacturer } = await loadFixture(
-      deployTokenFixture
-    );
+  describe("> manufacturer", () => {
+    it("sets the manufacturer as the owner of the contract", async () => {
+      const { testedContract, manufacturer } = await loadFixture(
+        deployTokenFixture
+      );
 
-    expect(await testedContract.manufacturer()).to.equal(manufacturer.address);
+      expect(await testedContract.manufacturer()).to.equal(
+        manufacturer.address
+      );
+    });
+
+    it("allows the owner of the car to transfer ownership", async () => {
+      const { testedContract, owner } = await loadFixture(deployTokenFixture);
+
+      await testedContract.transfer(0, owner.address);
+
+      const expectedOwner = await testedContract.getOwner(0);
+
+      expect(expectedOwner).to.equal(owner.address);
+    });
   });
 
   async function deployTokenFixture() {
